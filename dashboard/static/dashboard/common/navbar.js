16eded9bad4f9296c9341d0bbe02cf207db43aa4
Florent <florent.butery@isen-ouest.yncrea.fr>
2025-02-24 10:21:06 -0500

    use variable + enhancement + use navabar

--- dashboard/static/dashboard/common/navbar.js
+++ dashboard/static/dashboard/common/navbar.js
@@ -1,35 +1,3 @@
-// Navbar buttons
-const navbarDashboard = document.querySelector('.navbar-Dashboard');
-const navbarButtons = document.querySelectorAll('.navbar-onglets-bloc');
-const pageNow = navbarDashboard
-
-navbarButtons.forEach(button => {
-        button.addEventListener('mousedown', function () {
-            // Appliquer le style actif au bouton cliqué
-            this.style.backgroundColor = "white";
-            this.style.color = "black";
-            location.reload();
-
-            console.log(this.textContent.trim() + " sélectionné !");
-        });
-
-        button.addEventListener('mouseover', function () {
-            this.style.backgroundColor = "#1E5EBC";
-            this.style.color = "white";
-        });
-
-        button.addEventListener('mouseout', function () {
-            if (this !== pageNow) {
-                this.style.backgroundColor = "#f0f0f0";
-                this.style.color = "black";
-            }else {
-                this.style.backgroundColor = "#3585F9";
-                this.style.color = "white";
-            }
-
-        });
-    });
-
 // Navbar burger
 const navbarBurger = document.querySelector('.navbar-burger');
 const dropdownMenu = document.querySelector('.dropdown-menu');
