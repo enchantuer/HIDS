eabe74b8a177a644c4abd4ede7b72079ca636f3f
Goustan Sermon <goust.sermon-thuillier@outlook.fr>
2025-02-24 08:38:26 -0500

    Create analyse_yara.py

---
+++ local/analyse_yara.py
@@ -0,0 +1,34 @@
+import yara
+from scapy.all import rdpcap, wrpcap, Raw
+
+def is_malicious(packet):
+    if packet.haslayer(Raw):  #Check if packets contain a payload
+        payload = bytes(packet[Raw].load)  #Extract payload
+        matches = rules.match(data=payload)  #Apply yara rules
+        if matches:
+            print(f"YARA detected {matches} in a packet")
+            return True
+    return False
+
+# Load yara rules
+rules = yara.compile(filepath="ADD PATH TO YARA RULES FILE HERE")
+
+# Load pcap file
+packets = rdpcap("ADD PATH TO PCAP FILE HERE")
+
+# Save suspicious packets path
+path_results = "ADD PATH TO A SAVE DESTINATION"
+
+suspicious_packets = []
+
+# Get suspicious_packets
+for pkt in packets:
+    if is_malicious(pkt):
+        suspicious_packets.append(pkt)
+
+# Save suspicious packets in a file
+if suspicious_packets:
+    wrpcap(path_results, suspicious_packets)
+    print(f"Extraction done, {len(suspicious_packets)} suspicious packets save in {path_results}")
+else:
+    print("No suspicious packets found")
