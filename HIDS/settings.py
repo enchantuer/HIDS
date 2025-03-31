ffb953fb1dfc7e391b30fb64ed903ac4dd4abfed
Florent <florent.butery@isen-ouest.yncrea.fr>
2025-03-30 20:23:22 -0400

    Add websocket
    Add support
    Add routing
    Add handling by models

--- HIDS/settings.py
+++ HIDS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,6 +74,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'HIDS.wsgi.application'
 
+# Backend pour WebSockets
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -128,3 +135,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Daphne
+ASGI_APPLICATION = "HIDS.asgi.application"
--- api/models.py
+++ api/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+def get_stats():
+    return {
+        "agents_count": Agent.objects.all().count(),
+        "agents_down_count": Agent.objects.filter(down=True).count(),
+        "alerts_count": Alert.objects.all().count(),
+    }
+
+def send_stats(channel_layer):
+    async_to_sync(channel_layer.group_send)(
+        "stats",
+        {
+            "type": "send_stats",
+            "message": get_stats(),
+        }
+    )
 
 # Create your models here.
 class Agent(models.Model):
@@ -10,6 +28,14 @@ class Agent(models.Model):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        channel_layer = get_channel_layer()
+        send_stats(channel_layer)
+
+
+
 class Alert(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
@@ -18,6 +44,41 @@ class Alert(models.Model):
     description = models.CharField(max_length=200)
     level = models.IntegerField()
     pcap = models.FileField(upload_to="static/alerts/%y/%m/%d", null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        is_update = self.pk is not None and Alert.objects.filter(pk=self.pk).exists()
+
+        super().save(*args, **kwargs)
+
+        # Envoyer une notification WebSocket
+        channel_layer = get_channel_layer()
+
+        async_to_sync(channel_layer.group_send)(
+            "alerts",
+            {
+                "type": "send_alert",
+                "message": {
+                    "alert": self.to_dict(),
+                    "operation": "update" if is_update else "create",
+                }
+            }
+        )
+
+        send_stats(channel_layer)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "agent__id": self.agent.id,
+            "agent__name": self.agent.name,
+            "source": self.source,
+            "type": self.type,
+            "description": self.description,
+            "level": self.level,
+            "pcap": self.pcap.url if self.pcap else None,
+        }
+
 
     def __str__(self):
         return str(self.id)
