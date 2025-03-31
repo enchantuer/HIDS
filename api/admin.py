9279350429c6024f7f6dc102be4d951479143863
Florent <florent.butery@isen-ouest.yncrea.fr>
2025-03-30 22:52:11 -0400

    Bug fix
    Add LogNbRequest model to default admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from api.models import Agent, Alert
+from api.models import Agent, Alert, LogNbRequest
 
 # Register your models here.
 admin.site.register(Agent)
 admin.site.register(Alert)
+admin.site.register(LogNbRequest)
