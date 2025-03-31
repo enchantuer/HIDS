db56c83506bad4ff355d633dbfe2002d35039800
enchantuer <31900604+enchantuer@users.noreply.github.com>
2025-03-31 02:25:13 -0400

    Delete api directory

--- api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from api.models import Agent, Alert, LogNbRequest
-
-# Register your models here.
-admin.site.register(Agent)
-admin.site.register(Alert)
-admin.site.register(LogNbRequest)
--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("agents", views.get_agents, name="api_agents"),
-    path("alerts", views.get_alerts, name="api_alerts"),
-    path("stats", views.get_stats, name="api_stats"),
-]
