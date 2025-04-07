6b71479bfb801c69cef2101cc7dbcd73e8c4dd95
Florent <florent.butery@isen-ouest.yncrea.fr>
2025-04-07 09:55:46 -0400

    fix urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.redirect_to_dashboard, name='redirect_root'),
-    path("dashboard", views.index, name="dashboard"),
-    path("information", views.info, name="info")
+    path("dashboard/", views.index, name="dashboard"),
+    path("information/", views.info, name="info")
 ]
