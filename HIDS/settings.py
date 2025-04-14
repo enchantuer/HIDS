fe731a7db4ff4058d17d06260ac9cc86fcd927c8
Florent <florent.butery@isen-ouest.yncrea.fr>
2025-04-14 11:22:18 -0400

    add comment for env setting

--- HIDS/settings.py
+++ HIDS/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,11 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-12ro2-!jvhfz7+(ptu-)^qc+5-545o$i!xznpxfcptce*h-!qo'
+# SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = bool(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', "127.0.0.1").split(',')
 
 
 # Application definition
@@ -90,6 +93,16 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.{}'.format(
+    #         os.getenv('DATABASE_ENGINE', 'sqlite3')
+    #     ),
+    #     'NAME': os.getenv('DATABASE_NAME', 'polls'),
+    #     'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
+    #     'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+    #     'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+    #     'PORT': os.getenv('DATABASE_PORT', 5432),
+    # }
 }
 
 
